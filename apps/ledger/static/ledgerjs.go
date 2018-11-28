78dbe93f18cd309f91c42d03f15312c47152472c
Travis Keep <keep94@gmail.com>
2018-11-28 13:24:06 -0800

    In static package break static.go up into smaller files.

---
+++ apps/ledger/static/ledgerjs.go
@@ -0,0 +1,62 @@
+package static
+
+var (
+  kLedgerJs = `
+function selectAll(checkAll, className) {
+  checkboxes = checkAll.form.getElementsByClassName(className);
+  for (var idx = 0; checkboxes[idx]; idx++) {
+    checkboxes[idx].checked = checkAll.checked;
+  }
+}
+
+function populateSelect(select, options) {
+  for (var idx = 0; options[idx]; idx += 2) {
+    var o = document.createElement("option");
+    o.value = options[idx];
+    o.text = options[idx + 1];
+    select.add(o, null);
+  }
+}
+
+function initAutoComplete(autoComplete) {
+  autoComplete.applyLocalFilter = true;
+  autoComplete.queryMatchContains = true;
+  autoComplete.autoHighlight = false;
+}
+
+function Suggester(url) {
+  this._url = url;
+  this._suggestions = [];
+}
+
+Suggester.prototype._initRequest = function() {
+  if (window.isIE) {
+    return new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  return new XMLHttpRequest();
+};
+
+Suggester.prototype.useUrl = function(url) {
+  this._url = url;
+  return this;
+}
+
+Suggester.prototype.getSuggestions = function() {
+  if (this._url !== this._readUrl) {
+    this._readUrl = this._url;
+    this._suggestions = [];
+    var req = this._initRequest();
+    var that = this;
+    req.onreadystatechange = function() {
+      if (req.readyState == 4) {
+        if (req.status == 200) {
+          that._suggestions = JSON.parse(req.responseText);
+        }
+      }
+    };
+    req.open("GET", this._url, true);
+    req.send(null);
+  }
+  return this._suggestions;
+};`
+)
---
+++ apps/ledger/static/themecss.go
@@ -0,0 +1,9 @@
+package static
+
+var (
+  kThemeCss = `
+.positive {color:#006600;}
+.negative {color:#660000;}
+.lineitem {background-color:#CCCCCC}
+.error {color:#FF0000;font-weight:bold;}`
+)
