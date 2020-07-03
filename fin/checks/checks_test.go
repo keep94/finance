49318455e8d405744f69d3dd6863ceb27debdb95
Travis Keep <keep94@gmail.com>
2020-07-03 16:24:24 -0700

    Add more tests to fin/checks package.
    Minor fixes to existing tests in same package.

--- fin/checks/checks_test.go
+++ fin/checks/checks_test.go
@@ -14,16 +14,33 @@ func TestNoChecks(t *testing.T) {
 
 func TestMissingChecks(t *testing.T) {
   assert := assert.New(t)
-  missing := checks.Missing([]int{1003, 1001, 1007, 1004, 1008, 1009, 1001, 1004})
+  missing := checks.Missing([]int{1003, 1001, 1007, 1004, 1009, 1001, 1004})
   assert.Equal(1001, missing.First)
   assert.Equal(1009, missing.Last)
-  assert.Equal(missing.Holes, []checks.Range{{1002, 1002}, {1005, 1006}})
+  assert.Equal(
+      []checks.Range{{1002, 1002}, {1005, 1006}, {1008, 1008}}, missing.Holes)
 }
 
-func TestNoHoles(t *testing.T) {
+func TestSingle(t *testing.T) {
   assert := assert.New(t)
   missing := checks.Missing([]int{101})
   assert.Equal(101, missing.First)
   assert.Equal(101, missing.Last)
-  assert.Equal(0, len(missing.Holes))
+  assert.Empty(missing.Holes)
+}
+
+func TestOneBigHole(t *testing.T) {
+  assert := assert.New(t)
+  missing := checks.Missing([]int{3000, 2000})
+  assert.Equal(2000, missing.First)
+  assert.Equal(3000, missing.Last)
+  assert.Equal([]checks.Range{{2001, 2999}}, missing.Holes)
+}
+
+func TestNoHoles(t *testing.T) {
+  assert := assert.New(t)
+  missing := checks.Missing([]int{101, 102, 103, 104})
+  assert.Equal(101, missing.First)
+  assert.Equal(104, missing.Last)
+  assert.Empty(missing.Holes)
 }
